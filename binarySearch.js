ae021e97470613bf12a5e803f2a4a33fd3824340
happy-programming1991 <happy_programming@aliyun.com>
2019-09-22 12:48:25 +0800

    chore

--- binarySearch.js
+++ binarySearch.js
@@ -4,8 +4,8 @@ function binarySearch(list, item) {
   let high = list.length - 1
 
   while (low <= high) {
-    let mid = Math.floor( (low + high) / 2 )
-    let guess = list[mid]
+    const mid = Math.floor( (low + high) / 2 )
+    const guess = list[mid]
 
     if (guess === item) {
       return mid
